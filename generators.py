4079e9cf72db6dda07031cddf4a8b6c4c1fc1bc2
Rajan <rajan2275@gmail.com>
2017-12-25 12:31:14 +0000

    generators

--- generators.py
+++ generators.py
@@ -1,13 +1,24 @@
-# -----------------------------------------
-# add 1 to list of numbers using generators
-# ------------------------------------------
+# ------------------------------------------------------
+# Example 1: add 1 to list of numbers using generators
+# ------------------------------------------------------
 
 def add_one(numbers):
     for num in numbers:
         yield(num + 1)
 
-
 numbers = [1, 2, 3, 4, 5]
 
-for num in add_one(numbers):  # DO NOT CONVERT numbers (e.g. list(number) TO LIST AS PERFORMANCE GAIN WILL BE LOST.
-    print(num)
+print('---------Start Example 1----------')
+for num in add_one(numbers):               # DO NOT CONVERT numbers (e.g. list(number) TO LIST AS PERFORMANCE GAIN WILL BE LOST.
+    print('Example 1:'+ str(num))
+
+
+# ------------------------------------------------------
+# Example 2: add 1 to list of numbers using generators
+# ------------------------------------------------------
+
+numbers = (x+1 for x in [1, 2, 3, 4, 5])   # replacing [] with () makes it generator.
+                                           # DO NOT CONVERT numbers (e.g. list(number) TO LIST AS PERFORMANCE GAIN WILL BE LOST.
+print('---------Start Example 2----------')
+for num in numbers:
+    print( 'Example 2:'+ str(num))
