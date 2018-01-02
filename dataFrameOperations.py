d8eeb48708e95e8e57e122ba605a3e53925e8426
rajan <rajan2275@gmail.com>
2018-01-02 13:42:05 +0000

    DataFrame operations.

---
+++ dataFrameOperations.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+
+# ------------------------
+# Creating Empty DataFrame
+# ------------------------
+df = pd.DataFrame()
+print(df)
+print('----------------------------------')
+# ----------------------------
+# Creating DataFrame From Lists
+# ----------------------------
+print('Creating DataFrame From Lists')
+print('----------------------------------')
+
+df = pd.DataFrame(data =[['Employee1', 1000],['Employee2', 2000],['Employee3', 4000]],
+                  columns=['Name', 'Salary'])
+print(df)
+print('----------------------------------')
+
+# ----------------------------------
+# Creating DataFrame From Dictionary
+# ----------------------------------
+print('Creating DataFrame From Dictionary')
+print('----------------------------------')
+data = {'Name': ['Employee1','Employee2', 'Employee3'], 'Salary':[1000, 2000, 3000]}
+df = pd.DataFrame(data)
+print(df)
+print('----------------------------------')
+
+print(df.describe())
